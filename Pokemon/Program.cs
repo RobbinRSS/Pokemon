7adf1a4e62f73f4654e2dc72ce75b1a5263992fe
Robbin Schrijver <robbinschrijver07@gmail.com>
2025-02-14 10:01:21 +0100

    ASSIGNEMENT 2 FINISHED, added Trainer class and Pokeball class with throw and return pokeball functions

--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -31,29 +31,98 @@ namespace pokemon
         }
     }
 
+    class Pokeball
+    {
+        public Charmander charmander;
+        public bool isOpen;
+
+        public Pokeball(Charmander charmander)
+        {
+            this.charmander = charmander;
+            isOpen = false;
+        }
+
+        public void Throw()
+        {
+            if (isOpen){
+                Console.WriteLine($"{charmander.name} was already outside his ball");
+            }
+            else
+            {
+                isOpen = true;
+                Console.WriteLine($"{charmander.name} is thrown");
+            }
+        }
+
+        public void Return()
+        {
+            if (isOpen)
+            {
+                isOpen = false;
+                Console.WriteLine($"{charmander.name} returned to his ball");
+            } else
+            {
+                Console.WriteLine($"{charmander.name} was already in his ball");
+            }
+        }
+
+    }
+
+    class Trainer
+    {
+        public string name;
+        public List<Pokeball> belt = new List<Pokeball>();
+
+        public Trainer(string name)
+        {
+            this.name = name;
+            for (int i = 0; i < 6; i++)
+            {
+                belt.Add(new Pokeball(new Charmander($"Charmander {i + 1}", "Char Char")));
+            }
+        }
+
+        public void ThrowPokeball(int index)
+        {
+            if (index < belt.Count)
+            {
+                Console.WriteLine($"{name} throws Pokeball {index + 1}!");
+                belt[index].Throw();
+            }
+        }
+
+        public void ReturnPokeball(int index)
+        {
+            if (index < belt.Count)
+            {
+                Console.WriteLine($"{name} returns Pokeball {index + 1}!");
+                belt[index].Return();
+            }
+        }
+
+    }
+
     class Program
     {
 
         public static void Main(string[] args)
-        {        
-            Charmander charmander1 = new Charmander("Charmander", "Char Char");
+        {
+            Console.WriteLine("Geef je eerste trainer een naam");
+            string trainer1Name = Console.ReadLine();
+            Trainer trainer1 = new Trainer(trainer1Name);
 
-            charmander1.SetName();
-            charmander1.BattleCry();
+            Console.WriteLine("Geef je tweede trainer een naam");
+            string trainer2Name = Console.ReadLine();
+            Trainer trainer2 = new Trainer(trainer2Name);
 
-            while (true)
+            for(int i = 0;i < 6; i++)
             {
-                Console.WriteLine("Wil je een nieuwe naam aan je charmander geven (ja/nee)");
-                string input = Console.ReadLine().ToLower();
-                
-                // guard clause
-                if (input == "nee") break;
-
-                charmander1.SetName();
-                charmander1.BattleCry();
-
-
+                trainer1.ThrowPokeball(i);
+                trainer2.ThrowPokeball(i);
+                trainer1.ReturnPokeball(i);
+                trainer2.ReturnPokeball(i);
             }
+
 
         }
     }
